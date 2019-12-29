ed249badcc81b71755693bfac881c76bad769d08
Saurabh Jadhav <saurabhjk07@gmail.com>
2019-12-29 14:00:28 +0530

    SA

---
+++ 3.c
@@ -0,0 +1,12 @@
+// { autofold
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+// }
+
+int main()
+{
+    // Write your code here.
+    // Click on "RUN" to execute it.
+    printf("Hello World!\n");
+}
